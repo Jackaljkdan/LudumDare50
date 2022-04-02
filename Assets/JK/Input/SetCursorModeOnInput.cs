b29f3eff00671086610b2227d488d43daa186532
Daniele Cortesi <dani.c93@hotmail.it>
2022-04-02 07:43:37 +0200

    Initial commit

---
+++ Assets/JK/Input/SetCursorModeOnInput.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace JK.Input
+{
+    [DisallowMultipleComponent]
+    public class SetCursorModeOnInput : MonoBehaviour
+    {
+        #region Inspector
+
+        public CursorLockMode mode = CursorLockMode.Locked;
+
+        public DestroyMode destroyMode = DestroyMode.DestroyGameObject;
+
+        #endregion
+
+        [Serializable]
+        public enum DestroyMode
+        {
+            None,
+            DestroySelf,
+            DestroyGameObject,
+        }
+
+        private void Update()
+        {
+            if (UnityEngine.Input.anyKeyDown)
+            {
+                Cursor.lockState = mode;
+
+                switch (destroyMode)
+                {
+                    case DestroyMode.DestroySelf:
+                        Destroy(this);
+                        break;
+                    case DestroyMode.DestroyGameObject:
+                        Destroy(gameObject);
+                        break;
+                }
+            }
+        }
+    }
+}
