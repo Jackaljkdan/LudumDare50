8151c30a5588b26fbc49b1f9ae15f46eb684bf5c
Daniele Cortesi <dani.c93@hotmail.it>
2022-04-02 18:34:27 +0200

    Regulate piss

--- Assets/Inevitable/Scripts/Piss.cs
+++ Assets/Inevitable/Scripts/Piss.cs
@@ -1,3 +1,4 @@
+using JK.Actuators.Input;
 using JK.World;
 using System;
 using System.Collections;
@@ -5,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
+using Zenject;
 
 namespace Inevitable
 {
@@ -16,6 +18,14 @@ namespace Inevitable
         public new Renderer renderer;
 
         public float secondsToBottom = 1;
+
+        public AnimationCurve curveToBottom;
+
+        public Transform follow;
+
+        public Vector3 followOffset = Vector3.zero;
+
+        public float lerp = 0.1f;
 
         [SerializeField]
         private List<GameObject> particles = null;
@@ -29,9 +39,59 @@ namespace Inevitable
 
         private List<ContactPoint> contactsList = new List<ContactPoint>(10);
 
-        private void Start()
+        private bool finishedToBottom = false;
+
+        //[Inject]
+        //private RotationActuatorInputBehaviour rotationInput = null;
+
+        private void OnEnable()
         {
-            renderer.material.SetFloat("Cutoff", 1);
+            StartCoroutine(PeeToBottomCoroutine());
+        }
+
+        private IEnumerator PeeToBottomCoroutine()
+        {
+            finishedToBottom = false;
+            float elapsed = 0;
+
+            while (elapsed < secondsToBottom)
+            {
+                renderer.material.SetFloat("Cutoff", curveToBottom.Evaluate(elapsed /secondsToBottom));
+                elapsed += Time.deltaTime;
+                yield return null;
+            }
+
+            finishedToBottom = true;
+        }
+
+        private void LateUpdate()
+        {
+            if (follow == null)
+                return;
+
+            Transform tr = transform;
+
+            tr.position = Vector3.Lerp(tr.position, follow.position + tr.TransformDirection(followOffset), lerp);
+
+            var followEuler = follow.eulerAngles;
+
+            var euler = tr.eulerAngles;
+            euler.y = Mathf.LerpAngle(euler.y, followEuler.y, lerp);
+            tr.eulerAngles = euler;
+
+            // x rot 0 => scale 4.7
+            // x rot 45 => scale 1
+
+            float rotX = followEuler.x;
+            if (rotX > 45)
+            {
+                if (rotX < 90)
+                    rotX = 45;
+                else if (rotX >= 90)
+                    rotX = 0;
+            }
+
+            tr.localScale = Vector3.Lerp(tr.localScale, new Vector3(1, 1, Mathf.Lerp(4.7f, 0.5f, rotX/45)), lerp);
         }
 
         private void OnCollisionStay(Collision collision)
@@ -49,7 +109,7 @@ namespace Inevitable
                         : -1
             );
 
-            Debug.Log($"contacts {string.Join(" ", contactsList.Select(cp => cp.point.y.ToString("0.0")))}");
+            //Debug.Log($"contacts {string.Join(" ", contactsList.Select(cp => cp.point.y.ToString("0.0")))}");
 
             for (int i = 0; i < particles.Count; i++)
             {
